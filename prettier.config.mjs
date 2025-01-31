c7ec4ccf0d2499cb6df548d022f07e8d53052fa6
Jacob Cassidy <jacob@jacobcassidy.com>
2025-01-31 16:03:52 +0700

    Add linting and formatting settings files

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * Prettier configuration.
+ * @see https://prettier.io/docs/en/configuration.html
+ */
+import wpConfig from '@wordpress/prettier-config';
+
+const config = {
+	...wpConfig,
+	printWidth: 120,
+};
+
+export default config;
