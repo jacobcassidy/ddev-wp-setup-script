8f04447d198033030c0568bbe48245fabfe0207f
Jacob Cassidy <jacob@jacobcassidy.com>
2025-04-04 11:24:04 +0700

    Update paths to modules

--- install.sh
+++ install.sh
@@ -56,17 +56,17 @@ ddev config --project-type=wordpress --project-name=$PROJECT_NAME_SLUG
 
 # Add Spatie Ray app files for development env.
 if $INSTALL_RAY_CONNECTIONS; then
-  source /modules/ray-app-connections-module.sh
+  source ${ROOT_DIR}/modules/ray-app-connections-module.sh
 fi
 
 # Add WordPress debugging log for WP development.
 if $INSTALL_WP_DEBUG_SETTING; then
-  source /modules/wp-debug-log-setup-module.sh
+  source ${ROOT_DIR}/modules/wp-debug-log-setup-module.sh
 fi
 
 # Add git and git assets.
 if $INSTALL_GIT; then
-  source /modules/git-local-setup-module.sh
+  source ${ROOT_DIR}/modules/git-local-setup-module.sh
 fi
 
 # Build and start the project's Docker containers.
@@ -74,4 +74,4 @@ printf "${BLUE}Starting DDEV containers...${RESET}\n"
 ddev start $PROJECT_NAME_SLUG
 
 # Install and set up WordPress for development.
-source /modules/wp-starter-setup-module.sh
+source ${ROOT_DIR}/modules/wp-starter-setup-module.sh
