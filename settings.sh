df5d01c47d27fef80481102fb927de9c7c587935
Jacob Cassidy <jacob@jacobcassidy.com>
2025-04-10 06:03:58 +0700

    Update organization of settings

--- settings.sh
+++ settings.sh
@@ -1,62 +1,31 @@
 #!/bin/bash
 
-# Your WP site name. Defaults to the project name taken from the project directory.
-# Uncomment the following line to set a custom name.
-# WP_SITE_TITLE='Example'
+# DETAILS
 
-# Your WP login details
+## Your WP site name. Defaults to the project name taken from the project directory.
+## Uncomment the following line to set a custom name.
+# WP_SITE_TITLE='Example Title'
+
+## WP Admin login details for user 1
 WP_USER_NAME='example-name'
 WP_USER_PASS='example-password'
-WP_USER_EMAIL='dev@example.com'
+WP_USER_EMAIL='example-email@example.com'
 
-# Theme Slugs
+## Theme Slugs
 DEFAULT_THEME_SLUG='twentytwentyfive'
 STARTER_THEME_SLUG='cassidywp-starter-block-theme'
 
-# Local path to the All-in-One WP Migration Unlimited Extension plugin zip file.
-# If you don't have this plugin, set `INSTALL_LOCAL_AIOMUE_PLUGIN` to false.
+## Local path to the All-in-One WP Migration Unlimited Extension plugin zip file.
+## Not used unless INSTALL_LOCAL_AIOMUE_PLUGIN is set to true.
 LOCAL_AIOMUE_PATH='/Users/Jacob/Projects/Assets/Packages/WordPress/Plugins/All In One Migration Unlimited Extension/all-in-one-wp-migration-unlimited-extension_2.65.zip'
 
-# Turn settings on/off with true/false values:
-
-## THEMES
-
-### Installs official Default WordPress Theme
-INSTALL_DEFAULT_THEME=true
-
-### Installs CassidyWP Starter Block Theme
-INSTALL_STARTER_THEME=true
-
-## PLUGINS
-
-### Installs All-in-One WP Migration plugin
-INSTALL_AIOM_PLUGIN=true
-
-### Installs All-in-One WP Migration Unlimited Extension plugin from local machine zip file
-INSTALL_LOCAL_AIOMUE_PLUGIN=false
-
-### Installs Query Monitor plugin
-INSTALL_QUERY_MONITOR_PLUGIN=true
-
-## WIDGETS
-
-### Hides default WordPress dashboard widgets for user 1
-HIDE_DASHBOARD_WIDGETS=true
-
-## FILES
-
-### Installs Spatie Ray connection files to work with Docker containers
-INSTALL_RAY_CONNECTIONS=true
-
-### Installs local Git repo and .gitignore file for the project
-INSTALL_GIT=true
+# SETTINGS - Turn settings on/off with true/false values
 
 ## DEBUGGING
-
-### Adds WordPress debugging and env settings
+### Add WordPress debugging and env settings
 INSTALL_WP_DEBUG_SETTING=true
-
-### Set values for WP debugging and env settings
+### Set values for WP debugging and env settings.
+### Only used if INSTALL_WP_DEBUG_SETTING is set to true.
 if $INSTALL_WP_DEBUG_SETTING; then
   WP_DEBUG_VALUE=true
   WP_DEBUG_DISPLAY_VALUE=false
@@ -65,3 +34,27 @@ if $INSTALL_WP_DEBUG_SETTING; then
   WP_ENVIRONMENT_TYPE_VALUE='development'
   WP_DEVELOPMENT_MODE_VALUE='theme'
 fi
+
+## FILES
+### Install Spatie Ray connection files to work with Docker containers
+INSTALL_RAY_CONNECTIONS=true
+### Install local Git repo and .gitignore file for the project
+INSTALL_GIT=true
+
+## PLUGINS
+### Install All-in-One WP Migration plugin
+INSTALL_AIOM_PLUGIN=true
+### Install All-in-One WP Migration Unlimited Extension plugin from local machine zip file
+INSTALL_LOCAL_AIOMUE_PLUGIN=false
+### Install Query Monitor plugin
+INSTALL_QUERY_MONITOR_PLUGIN=true
+
+## THEMES
+### Install official Default WordPress Theme
+INSTALL_DEFAULT_THEME=true
+### Install CassidyWP Starter Block Theme
+INSTALL_STARTER_THEME=true
+
+## WIDGETS
+### Hide default WordPress dashboard widgets for user 1
+HIDE_DASHBOARD_WIDGETS=true
