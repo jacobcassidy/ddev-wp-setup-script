e5accffa89c22f8400bb0325748629bbadf3ce6a
Jacob Cassidy <jacob@jacobcassidy.com>
2025-07-21 09:15:03 +0700

    Refactor and rename WP install module

--- modules/wp-install-module.sh
+++ modules/wp-install-module.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Download and install Wordpress
-printf "${BLUE}Downloading WordPress...${RESET}\n"
-
-if ddev wp core version> /dev/null 2>&1; then
-  printf "${BLACK}Wordpress is already downloaded. Skipping download.${RESET}\n\n"
-else
-  # Download WordPress without themes or plugins
-  ddev wp core download --skip-content
-  echo '' # new line
-fi
-
 # Install WordPress
 printf "${BLUE}Installing WordPress...${RESET}\n"
 
@@ -21,9 +10,90 @@ else
   echo '' # new line
 fi
 
+if $INSTALL_CLEAN_WP; then
+  # Hide default WordPress dashboard widgets
+  if $HIDE_DASHBOARD_WIDGETS; then
+    printf "${BLUE}Hiding default WordPress dashboard widgets...${RESET}\n"
+    ddev wp eval 'update_user_meta(
+        1,
+        "metaboxhidden_dashboard",
+        array(
+            "dashboard_right_now",
+            "dashboard_activity",
+            "dashboard_site_health",
+            "dashboard_quick_press",
+            "dashboard_primary"
+        )
+    );
+    '
+    # Hide the welcome panel
+    ddev wp user meta update 1 show_welcome_panel 0
+    echo '' # new line
+  fi
+
+  # Remove default Hello World post
+  if $REMOVE_WP_HELLO_POST; then
+    printf "${BLUE}Deleting WordPress' default 'Hello, World' post...${RESET}\n"
+    if ddev wp post exists 1 > /dev/null 2>&1; then
+      ddev wp post delete 1 --force
+      echo '' # new line
+    else
+      printf "${BLACK}Post with ID 1 does not exist. Skipping deletion.${RESET}\n\n"
+    fi
+  fi
+
+  # Remove default Sample Page
+  if $REMOVE_WP_SAMPLE_PAGE; then
+    printf "${BLUE}Deleting WordPress' default 'Sample Page'...${RESET}\n"
+    if ddev wp post exists 2 > /dev/null 2>&1; then
+      ddev wp post delete 2 --force
+      echo '' # new line
+    else
+      printf "${BLACK}Post with ID 2 does not exist. Skipping deletion.${RESET}\n\n"
+    fi
+  fi
+
+  # Remove default Privacy Policy draft page
+  if $REMOVE_WP_PRIVACY_DRAFT; then
+    printf "${BLUE}Deleting WordPress' default 'Privacy Policy'...${RESET}\n"
+    if ddev wp post exists 3 > /dev/null 2>&1; then
+      ddev wp post delete 3 --force
+      echo '' # new line
+    else
+      printf "${BLACK}Post with ID 3 does not exist. Skipping deletion.${RESET}\n\n"
+    fi
+  fi
+
+  # Update the WP permalinks to use the post name
+  if $SET_POSTNAME_PERMALINKS; then
+    printf "${BLUE}Setting WP permalinks to use '%%postname%%'...${RESET}\n"
+
+    if [[ $(ddev wp option get permalink_structure) == '/%postname%' ]]; then
+      printf "${BLACK}The permalink structure is already using '%%postname%%'. Skipping restructure.${RESET}\n\n"
+    else
+      ddev wp rewrite structure '/%postname%'
+      echo '' # new line
+    fi
+  fi
+
+  # Create a new page titled 'Homepage' and set it to be the site's front page
+  if $CREATE_HOMEPAGE; then
+    printf "${BLUE}Creating a new page titled 'Homepage' and setting it to be the site's front page...${RESET}\n"
+
+    if [[ $(ddev wp option get show_on_front) == 'page' ]]; then
+      printf "${BLACK}Homepage already exists. Skipping creation.${RESET}\n\n"
+    else
+      HOMEPAGE_ID=$(ddev wp post create --post_type=page --post_title='Homepage' --post_author=1 --post_status=publish --post_content='<!-- wp:paragraph --><p>This is the homepage.</p><!-- /wp:paragraph -->' --porcelain)
+      ddev wp option update show_on_front 'page'
+      ddev wp option update page_on_front $HOMEPAGE_ID
+      echo '' # new line
+    fi
+  fi
+fi
+
 # Install default theme
 if $INSTALL_DEFAULT_THEME; then
-  printf "${BLUE}Installing ${DEFAULT_THEME_SLUG} theme...${RESET}\n"
+  printf "${BLUE}Installing default ${DEFAULT_THEME_SLUG} theme...${RESET}\n"
 
   if ddev wp theme is-installed ${DEFAULT_THEME_SLUG} > /dev/null 2>&1; then
     DEFAULT_THEME_NAME=$(ddev wp theme get ${DEFAULT_THEME_SLUG} --field=name)
@@ -51,7 +121,6 @@ if $INSTALL_STARTER_THEME; then
     # Remove remote Git connection to CassidyWP Starter Block Theme repo.
     printf "${BLUE}Removing cloned remote Git for ${CUSTOM_THEME_NAME}...${RESET}\n"
     git -C wp-content/themes/${STARTER_THEME_SLUG} remote remove origin
-    # Print success message
     printf "${GREEN}Removed cloned remote Git connection for ${CUSTOM_THEME_NAME}.${RESET}\n\n"
   fi
 fi
@@ -98,76 +167,6 @@ if $INSTALL_QUERY_MONITOR_PLUGIN; then
     ddev wp plugin install $PLUGIN_SLUG_QM --activate
     echo '' # new line
   fi
-fi
-
-# Update the WP permalinks to use post name
-printf "${BLUE}Setting WP permalinks to use '%%postname%%'...${RESET}\n"
-
-if [[ $(ddev wp option get permalink_structure) == '/%postname%' ]]; then
-  printf "${BLACK}The permalink structure is already using '%%postname%%'. Skipping restructure.${RESET}\n\n"
-else
-  ddev wp rewrite structure '/%postname%'
-  echo '' # new line
-fi
-
-# Hide default WordPress dashboard widgets
-if $HIDE_DASHBOARD_WIDGETS; then
-  printf "${BLUE}Hiding default WordPress dashboard widgets...${RESET}\n"
-  ddev wp eval 'update_user_meta(
-      1,
-      "metaboxhidden_dashboard",
-      array(
-          "dashboard_right_now",
-          "dashboard_activity",
-          "dashboard_site_health",
-          "dashboard_quick_press",
-          "dashboard_primary"
-      )
-  );
-  '
-  # Hide the welcome panel
-  ddev wp user meta update 1 show_welcome_panel 0
-  echo '' # new line
-fi
-
-# Delete Hello World post
-# ddev wp post exists 1 > /dev/null 2>&1 && ddev wp post delete 1 --force
-printf "${BLUE}Deleting WordPress' default 'Hello, World' post...${RESET}\n"
-if ddev wp post exists 1 > /dev/null 2>&1; then
-  ddev wp post delete 1 --force
-  echo '' # new line
-else
-  printf "${BLACK}Post with ID 1 does not exist. Skipping deletion.${RESET}\n\n"
-fi
-
-# Delete Sample Page
-printf "${BLUE}Deleting WordPress' default 'Sample Page'...${RESET}\n"
-if ddev wp post exists 2 > /dev/null 2>&1; then
-  ddev wp post delete 2 --force
-  echo '' # new line
-else
-  printf "${BLACK}Post with ID 2 does not exist. Skipping deletion.${RESET}\n\n"
-fi
-
-# Delete Privacy Policy draft page
-printf "${BLUE}Deleting WordPress' default 'Privacy Policy'...${RESET}\n"
-if ddev wp post exists 3 > /dev/null 2>&1; then
-  ddev wp post delete 3 --force
-  echo '' # new line
-else
-  printf "${BLACK}Post with ID 3 does not exist. Skipping deletion.${RESET}\n\n"
-fi
-
-# Create a 'Homepage' page
-printf "${BLUE}Creating a new page titled 'Homepage' and setting it to be the site's front page...${RESET}\n"
-
-if [[ $(ddev wp option get show_on_front) == 'page' ]]; then
-  printf "${BLACK}Homepage already exists. Skipping creation.${RESET}\n\n"
-else
-  HOMEPAGE_ID=$(ddev wp post create --post_type=page --post_title='Homepage' --post_author=1 --post_status=publish --post_content='<!-- wp:paragraph --><p>This is the homepage.</p><!-- /wp:paragraph -->' --porcelain)
-  ddev wp option update show_on_front 'page'
-  ddev wp option update page_on_front $HOMEPAGE_ID
-  echo '' # new line
 fi
 
 # Open project in default browser to finish WordPress installation
